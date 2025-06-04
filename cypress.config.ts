4c3528148257b9f6f9921ec989ac8bec7f9b0af7
HayaAbuRaed <hayaa.aburaed@gmail.com>
2025-06-04 13:08:53 +0300

    EMT-2 Add cypress.env.json

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 import { setupNodeEvents } from "@plugins/setupNodeEvents";
+import env from "./cypress.env.json";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://devcobalt.restaurant365.com",
+    viewportWidth: 1600,
+    viewportHeight: 900,
+    baseUrl: env.baseUrl,
     specPattern: "**/*.feature",
     setupNodeEvents,
   },
