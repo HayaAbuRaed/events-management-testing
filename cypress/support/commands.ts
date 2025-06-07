0ff2c0b21408535541807cb7863ccfa27e6a47f7
HayaAbuRaed <hayaa.aburaed@gmail.com>
2025-06-08 01:42:42 +0300

    EMT-2 Fix login command to save the user ID as an enviroment variable

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,8 +2,7 @@
 
 import LoginPageActions from "cypress/e2e/login/pageObjects/actions";
 import { Credentials } from "./types";
-import { GetNetCoreUrlPrefix, GetServiceStackUrlPrefix } from "./utils";
-import { EMPTY_GUID } from "./constatnts";
+import { mainLogin } from "./utils";
 
 declare global {
   namespace Cypress {
@@ -38,51 +37,8 @@ Cypress.Commands.add("loginWithApi", (credentials?: Credentials) => {
     message: `Logging in with username: ${username}`,
   });
 
-  cy.request({
-    body: {
-      UserName: username,
-      Password: password,
-    },
-    method: "POST",
-    url: GetServiceStackUrlPrefix("auth/credentials?isFromWeb=true"),
-    headers: { Accept: "application/json, text/plain, */*" },
-    log: false,
-  }).then((response) => {
-    localStorage.setItem("userInfo", JSON.stringify(response.body));
-    cy.request({
-      headers: { Accept: "application/json" },
-      method: "GET",
-      url: GetServiceStackUrlPrefix(`Preferences/${EMPTY_GUID}`),
-      failOnStatusCode: false,
-      log: false,
-    });
-    cy.request({
-      headers: { Accept: "application/json" },
-      method: "GET",
-      url: GetServiceStackUrlPrefix(`SessionData/${response.body.SessionId}`),
-      failOnStatusCode: false,
-      log: false,
-    }).then((res) => {
-      localStorage.setItem("loggedInUserDetails", JSON.stringify(res.body));
-      cy.request({
-        method: "GET",
-        url: GetNetCoreUrlPrefix(`Permissions/User/${res.body.userId}`),
-        failOnStatusCode: false,
-        log: false,
-      });
-    });
-    cy.request({
-      method: "GET",
-      url: GetNetCoreUrlPrefix("User/NavigationPrivileges/v2"),
-      log: false,
-      failOnStatusCode: false,
-    }).then((res) => {
-      localStorage.setItem("navigationPrivileges", JSON.stringify(res.body));
-    });
-
-    Cypress.log({
-      name: "login status",
-      message: `user ${username} logged in successfully`,
-    });
+  mainLogin(username, password).then(() => {
+    const { userId } = JSON.parse(localStorage.getItem("loggedInUserDetails") || "");
+    Cypress.env("DEFAULT_USER_ID", userId);
   });
 });
--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,7 +1,56 @@
+import { EMPTY_GUID } from "./constatnts";
+
 export const GetServiceStackUrlPrefix = (url: string) => {
   return `${Cypress.env("serviceStack") || ""}${url}`;
 };
 
 export const GetNetCoreUrlPrefix = (url: string) => {
-    return `${Cypress.env("netCore") || ""}${url}`;
-}
+  return `${Cypress.env("netCore") || ""}${url}`;
+};
+
+export const mainLogin = (username: string, password: string): Cypress.Chainable => {
+  return cy
+    .request({
+      body: {
+        UserName: username,
+        Password: password,
+      },
+      method: "POST",
+      url: GetServiceStackUrlPrefix("auth/credentials?isFromWeb=true"),
+      headers: { Accept: "application/json, text/plain, */*" },
+      log: false,
+    })
+    .then((response) => {
+      localStorage.setItem("userInfo", JSON.stringify(response.body));
+      cy.request({
+        headers: { Accept: "application/json" },
+        method: "GET",
+        url: GetServiceStackUrlPrefix(`Preferences/${EMPTY_GUID}`),
+        failOnStatusCode: false,
+        log: false,
+      });
+      cy.request({
+        headers: { Accept: "application/json" },
+        method: "GET",
+        url: GetServiceStackUrlPrefix(`SessionData/${response.body.SessionId}`),
+        failOnStatusCode: false,
+        log: false,
+      }).then((res) => {
+        localStorage.setItem("loggedInUserDetails", JSON.stringify(res.body));
+        cy.request({
+          method: "GET",
+          url: GetNetCoreUrlPrefix(`Permissions/User/${res.body.userId}`),
+          failOnStatusCode: false,
+          log: false,
+        });
+      });
+      cy.request({
+        method: "GET",
+        url: GetNetCoreUrlPrefix("User/NavigationPrivileges/v2"),
+        log: false,
+        failOnStatusCode: false,
+      }).then((res) => {
+        localStorage.setItem("navigationPrivileges", JSON.stringify(res.body));
+      });
+    });
+};
